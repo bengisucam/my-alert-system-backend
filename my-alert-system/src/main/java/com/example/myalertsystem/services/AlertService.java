50615aa731a51775562454aee570a03771123ce6
Nur Bengisu <bengisu.cam@gmail.com>
2019-08-28 12:02:15 +0300

    without async and schedule

--- my-alert-system/src/main/java/com/example/myalertsystem/services/AlertService.java
+++ my-alert-system/src/main/java/com/example/myalertsystem/services/AlertService.java
@@ -6,6 +6,7 @@ import com.example.myalertsystem.model.Response;
 import lombok.var;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -53,8 +54,6 @@ public class AlertService {
     }
 
 
-    @Async
-    @Scheduled(fixedRate = 1000)
     public void getResponse(Alert alert) throws IOException {
 
         String desiredMethod = alert.getHttpMethod();
